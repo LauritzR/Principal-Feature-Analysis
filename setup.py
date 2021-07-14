0aa041dae634d39f99ea200d80d8b10a32e987a3
LauritzR <rasbachlauritz@googlemail.com>
2021-07-14 19:48:35 +0200

    Better imports

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pfa-pkg-Lauritz-Rasbach",
-    version="0.0.4",
+    version="0.1.1",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
