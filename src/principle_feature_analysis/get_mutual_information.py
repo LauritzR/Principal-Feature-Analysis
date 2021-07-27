6a0469e1202f0936c80143b4f536f608f3f7ce61
Lauritz Rasbach <35218654+LauritzR@users.noreply.github.com>
2021-07-27 14:08:46 +0200

    Update get_mutual_information.py

--- src/principle_feature_analysis/get_mutual_information.py
+++ src/principle_feature_analysis/get_mutual_information.py
@@ -1,19 +1,18 @@
-# Code published under creative commons license CC BY-NC-SA
 # Copyright with the authors of the publication "A principal feature analysis"
 
 import numpy as np
 import pandas as pd
 import math
 
-# Function to calculate the Shannon mutal information of features with one system state
-# Input:    data: a csv file in which the first row is the state of the system  and the other rows the values of the features
+# Function to calculate the Shannon mutal information of features with the components of the output function
+# Input:    data: a csv file in which the first number of components of the output function rows are the values of the components of the output function  and the other rows are the values of the features
 #           list_variables: sorted list of indices that refer to the features stored in the row of the csv-file
 #           min_n_datapoints_a_bin: is supposed to have the same value as min_n_datapoints_a_bin in the function find_relevant_principle_features
 #           basis_log_mutual_information:  the basis for the logarithm used to calculate the mutual information.
-# Output:   pd_mutual_information: pandas data frame where the first column refers to the indices of the features and the second column refers to the corresponding mutual information of the corresponding feature with the system state (row 0)
-#                                  The first entry is the dependence of the system state with itself. The further entries are the dependence of the corresponding row in the csv-file to the system state (first row of csv-file)
+# Output:   list_pd_mutual_information: list of pandas data frames where each the first column refers to the indices of the row in the csv (respectively, the variable of which the values are stored there) and the second column refers to the corresponding mutual information of the corresponding feature with the component of the output-function (indicated in the first row of first column)
+#                                  The first entry is the mutual information of the corresponding component of the output fuction with itself. The further entries are the mutual information of the corresponding row in the csv-file to the corresponding component of the output function
 
-def get_mutual_information(data,list_variables,min_n_datapoints_a_bin,basis_log_mutual_information):
+def get_mutual_information(data,number_output_functions,list_variables,min_n_datapoints_a_bin,basis_log_mutual_information):
     # Calulate the Shannon mutual information
     def make_summand_from_frequencies(x,y):
         if x == 0:
@@ -21,7 +20,9 @@ def get_mutual_information(data,list_variables,min_n_datapoints_a_bin,basis_log_
         else:
             return x * math.log2( x / y) / math.log2(basis_log_mutual_information)
 
-    list_variables.insert(0,0)
+    # Insert the the indices of the rows where the components of the output functions are stored
+    for i in range(0,number_output_functions):
+        list_variables.insert(i,i)
 
     data_init = data.to_numpy()
     data=data_init[list_variables,:]
@@ -29,16 +30,16 @@ def get_mutual_information(data,list_variables,min_n_datapoints_a_bin,basis_log_
     n = data.shape[1]
     l = [0 for i in range(0, m)]
     freq_data = [0 for i in range(0, m)]
-    left_metrics = [i for i in range(0, m)]
-    constant_metrics = []
+    left_features = [i for i in range(0, m)]
+    constant_features = []
 
     for i in range(0, m):
         mindata = min(data[i, :])
         maxdata = max(data[i, :])
         if maxdata <= mindata:
             print("Variable #"f"{i}" " has only constant values")
-            left_metrics.remove(i)
-            constant_metrics.append(i)
+            left_features.remove(i)
+            constant_features.append(i)
         else:
             list_points_of_support=[]
             datapoints=data[i,:].copy()
@@ -62,21 +63,31 @@ def get_mutual_information(data,list_variables,min_n_datapoints_a_bin,basis_log_
                     list_points_of_support.pop(-2)
             l[i]=list_points_of_support
             freq_data[i] = np.histogram(data[i, :], bins=l[i])[0]
-    #Check for constant metrics
-    if constant_metrics != []:
-        print("List of metrics with constant values:")
-        print(constant_metrics)
-    if 0 in constant_metrics or len(freq_data[0])<2:
-        print("Warning: System state is constant!")
+    #Check for constant features
+    if constant_features != []:
+        print("List of features with constant values:")
+        print(constant_features)
+    for id_output in range(0,number_output_functions):
+        if id_output in constant_features or len(freq_data[id_output]) < 2:  # Warn if the output function is constant e.g. due to an unsuitable binning
+            print("Warning: Output function " + str(id_output) +  " is constant!")
 
-    # Calculate the dependency measure for each feature with the system state
-    mutual_info = np.ones((1, len(left_metrics)))
-    for i in range(0,1):
-        for j in range(0,len(left_metrics)):
-            freq_data_product = ((np.histogram2d(data[i,:], data[left_metrics[j],:], bins=(l[i], l[left_metrics[j]]))[0]))/n
-            expfreq = (np.outer(freq_data[i], freq_data[left_metrics[j]]))/(n*n)
-            mutual_info[0,j]=np.sum(np.array(list(map(make_summand_from_frequencies,freq_data_product.flatten().tolist(),expfreq.flatten().tolist()))))
+    # Calculate the mutual information for each feature with the corresponding component of the output function
+    list_of_data_frames=[]
+    mutual_info = np.ones((1, len(left_features) - number_output_functions + 1))  # number of featuers plus one component of the output-function
+    for i in range(0,number_output_functions):
+        list_of_features=list(range(number_output_functions,len(left_features)))
+        list_of_features.insert(0,i)
+        id_features=list_variables[number_output_functions:]
+        id_features.insert(0,i)
+        for j in list_of_features:
+            freq_data_product = ((np.histogram2d(data[i,:], data[left_features[j], :], bins=(l[i], l[left_features[j]]))[0])) / n
+            expfreq = (np.outer(freq_data[i], freq_data[left_features[j]])) / (n * n)
+            if j<number_output_functions:
+                mutual_info[0, 0] = np.sum(np.array(list(map(make_summand_from_frequencies, freq_data_product.flatten().tolist(),expfreq.flatten().tolist()))))
+            else:
+                mutual_info[0,j-number_output_functions+1]=np.sum(np.array(list(map(make_summand_from_frequencies,freq_data_product.flatten().tolist(),expfreq.flatten().tolist()))))
+        pd_mutual_information = pd.DataFrame({"index feature": id_features, "mutual information": mutual_info.tolist()[0]})
+        pd_mutual_information['index feature'] = pd_mutual_information['index feature'].astype(int)
+        list_of_data_frames.append(pd_mutual_information)
 
-    pd_mutual_information=pd.DataFrame({"index feature" : list_variables,"mutual information":mutual_info.tolist()[0]})
-    pd_mutual_information['index feature']=pd_mutual_information['index feature'].astype(int)
-    return pd_mutual_information
+    return list_of_data_frames
