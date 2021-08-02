8b08d4168eff54cc9ad4e6575c8b1002a2c9f945
Lauritz Rasbach <35218654+LauritzR@users.noreply.github.com>
2021-08-02 15:35:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pfa-pkg-Lauritz-Rasbach",
-    version="0.1.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    name="principal-feature-analysis",
+    version="0.0.1",
+    author="Tim Breitenbach & Lauritz Rasbach",
+    author_email="tim.breitenbach@mathematik.uni-wuerzburg.de & rasbachlauritz@googlemail.com",
+    description="The first package for Principal Feature Analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD 3-Clause License",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
